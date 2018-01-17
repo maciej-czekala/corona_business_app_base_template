b34ab87ebf432dc310799293144fbbfa4a9ebd83
mczekala-insys <m.czekala@insys.pl>
2018-01-17 09:01:49 +0100

    fix for start up crach

--- app/scenes/home/home_view.lua
+++ app/scenes/home/home_view.lua
@@ -7,23 +7,23 @@ local function View(controler, model)
 
 	local bg = display.newRect( sceneGroup, _w*.5, _h*.5, _w, _h )
 
-	local title = display.newText( {parent = sceneGroup, text = model:get("title"), x = bg.x, y = 100} )
+	local title = display.newText( {parent = sceneGroup, text = model.get("title"), x = bg.x, y = 100} )
 	title:setFillColor( 0 )
 
-	local count = display.newText( {parent = sceneGroup, text = model:get("count"), x = bg.x, y = 150} )
+	local count = display.newText( {parent = sceneGroup, text = model.get("count"), x = bg.x, y = 150} )
 	count:setFillColor( 0 )
 
-	local is_active = display.newText( {parent = sceneGroup, text = tostring(model:get("is_active")), x = bg.x, y = 200} )
+	local is_active = display.newText( {parent = sceneGroup, text = tostring(model.get("is_active")), x = bg.x, y = 200} )
 	is_active:setFillColor( 0 )
 
 
 	-- set binding to model item to recieve callback if model item is updated
-	model:setBinding("count",
+	model.setBinding("count",
 		function(value)
 			count.text = value
 		end)
 
-	model:setBinding("is_active",
+	model.setBinding("is_active",
 		function(value)
 			is_active.text = tostring(value)
 		end)
